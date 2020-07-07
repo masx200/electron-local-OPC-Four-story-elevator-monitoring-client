7b7a372fa063f7e55d45ed4a16796635f1bfe37d
masx200 <34191203+masx200@users.noreply.github.com>
2020-07-07 13:39:13 +0800

    Update index.js

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,30 +1,20 @@
 "use strict";
 import "./renderer.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "popper.js/dist/esm/popper.min";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import Vue from "vue/dist/vue.esm.browser.min";
-
-import template from './template.js'
-
-
-import start from './start.js'
-
-
-
-
+import template from "./template.js";
+import start from "./start.js";
 (function init() {
-  (Vue.config.devtools = false),
-    (Vue.config.productionTip = false),
-    new Vue({
-      data: {},
-      methods: {},
-      template:template,
-      mounted() {
-      start()
-      },
-    }).$mount("#app");
+  Vue.config.devtools = false;
+  Vue.config.productionTip = false;
+  new Vue({
+    data: {},
+    methods: {},
+    template: template,
+    mounted() {
+      start();
+    },
+  }).$mount("#app");
 })();
-//window.nodeRequire = window.require;
