c1c2e06cee4dd442191f9b270f4197dece4e47a2
masx200 <34191203+masx200@users.noreply.github.com>
2020-07-07 13:08:29 +0800

    Update cmd.js

--- src/renderer/cmd.js
+++ src/renderer/cmd.js
@@ -1,32 +1,20 @@
-import { exec } from 'child_process';
-
-var commandline={
-    get:getString,
-    run:runCommand
+import { exec } from "child_process";
+var commandline = {
+  get: getString,
+  run: runCommand,
 };
-
-function runCommand(command){
-    //return refrence to the child process
-    return exec(
-        command
-    );
+function runCommand(command) {
+  return exec(command);
 }
-
-function getString(command,callback){
-    //return refrence to the child process
-    return exec(
-        command,
-        (
-            function(){
-                return function(err,data,stderr){
-                    if(!callback)
-                        return;
-
-                    callback(err, data, stderr);
-                }
-            }
-        )(callback)
-    );
+function getString(command, callback) {
+  return exec(
+    command,
+    (function () {
+      return function (err, data, stderr) {
+        if (!callback) return;
+        callback(err, data, stderr);
+      };
+    })(callback)
+  );
 }
-
 export default commandline;
